ee4c3f6718356ebcd989319abe392004adfdfe56
EduardoVila <eduardodevila1@hotmail.com>
2020-10-06 11:36:22 -0300

    Creating the menu component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,28 +1,8 @@
 <template>
   <div class="game-background">
-    <div class="counter" :class="!started && 'hidden'">SCORE: {{counter}}</div>
+    <div class="scoreCounter" :class="!started && 'hidden'">SCORE: {{scoreCounter}}</div>
 
-    <div class="main-section" :class="started && 'hidden'">
-      <div>
-        <div class="game-title">
-          <h1>
-            <span class="red-color">S</span><span class="blue-color">U</span><span class="yellow-color">P</span><span class="blue-color">E</span><span class="green-color">R</span> 
-            <span class="green-color">M</span><span class="blue-color">A</span><span class="yellow-color">R</span><span class="red-color">I</span><span class="green-color">O</span>
-          </h1>
-          <h1>
-            <span class="yellow-color">G</span><span class="blue-color">A</span><span class="green-color">M</span><span class="red-color">E</span>
-          </h1>
-        </div>
-
-        <div v-if="counter > 0" class="counter">YOUR SCORE: {{counter}}</div>
-
-        <div class="action-buttons">
-          <button @click="startGame" class="start-button">{{counter == 0 ? 'START GAME' : 'TRY AGAIN'}}</button>
-          <button v-if="counter > 0" class="ranking-button">SAVE RESULT</button>
-          <button class="ranking-button">RANKING</button>
-        </div>
-      </div>
-    </div>
+    <Menu :hidden="started" :startGameCallBack="startGame" :scoreCounter="scoreCounter" />
 
     <div class="game-area">
       <img ref="character" src="../assets/gifs/mario.gif" class="character" :class="started && 'started'">
@@ -31,14 +11,21 @@
   </div>
 </template>
 <script>
+
+import Menu from './Menu'
+
 export default {
   data() {
     return {
-      counter: 0,
+      scoreCounter: 0,
       level: 1,
       oldLevel: 1,
       started: false
     }
+  },
+  
+  components: {
+    Menu
   },
 
   mounted() {
@@ -48,6 +35,7 @@ export default {
 
   updated() {
     this.updateLevel()
+
     if (this.level != this.oldLevel) {
       this.updateAnimationSpeed()
 
@@ -59,16 +47,15 @@ export default {
     checkGameState() {
       setInterval((() => {
         if (!this.started) return
-
+        
         const blockXPosition = this.getPosition(this.$refs.block).xPos;
 
         if  (blockXPosition > 200 && blockXPosition < 300 && this.getPosition(this.$refs.character).yPos > 570 ) {
           this.started = false
-      } else {
-        this.counter++
-
-        this.updateLevel()
-      }
+        } else {
+          this.scoreCounter++
+          this.updateLevel()
+        }
       }).bind(this), 15)
     },
 
@@ -84,54 +71,52 @@ export default {
       if (!this.started || this.$refs.character.classList == 'jump') return
       
       this.$refs.character.classList.add('jump')
-
       setTimeout(() => {
         this.$refs.character.classList.remove('jump')
       }, 300)
     },
 
     updateLevel() {
-      if (this.counter > 1000) {
-        this.level = 2;
-      } else if (this.counter > 2000) {
-        this.level = 3;
-      } else if (this.counter > 3000) {
-        this.level = 4;
+      if (this.scoreCounter > 1000) {
+        this.level = 2
+      } else if (this.scoreCounter > 2000) {
+        this.level = 3
+      } else if (this.scoreCounter > 3000) {
+        this.level = 4
       }
     },
 
     updateAnimationSpeed() {
-      let speed = 0;
+      let speed = 0
 
       if (this.level == 1) {
-        speed = 3;
+        speed = 3
       } else if (this.level == 2) {
-        speed = 2.5;
+        speed = 2.5
       } else if (this.level == 3) {
-        speed = 2;
+        speed = 2
       } else if (this.level == 4) {
         speed = 1
       }
       
       const interval = setInterval((() => {
-        if (this.getPosition(this.$refs.block).xPos > -100) return;
+        if (this.getPosition(this.$refs.block).xPos > -100) return
 
         this.$refs.block.style.animationDuration = `${speed}s`
 
         clearInterval(interval)
       }).bind(interval), 5)
     },
-
     getPosition(element) {
       return {
         xPos: element.offsetLeft - element.scrollLeft + element.clientLeft,
         yPos: element.offsetTop - element.scrollTop + element.clientTop
       }
     },
-
     startGame() {
       this.$refs.block.style.animation = 'none'
-      this.started = true;
+      this.scoreCounter = 0
+      this.started = true
     }
   }
 }
@@ -165,7 +150,7 @@ export default {
       left 0px bottom 0px;
     animation: back 2s infinite linear;
 
-    .counter {
+    .scoreCounter {
       position: fixed;
       top: 1.5rem;
       right: 1.5rem;
@@ -173,63 +158,6 @@ export default {
       font-size: 1.5rem;
       font-weight: bold;
       color: #FFFFFF;
-    }
-
-    .main-section {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-
-      .counter {
-        position: initial;
-        text-align: center;
-        margin-top: 1rem;
-        margin-bottom: 1.5rem;
-        font-size: 1.6rem;
-      }
-
-      .game-title {
-        margin-top: 5rem;
-
-        h1 span {
-          font-family: 'Super Mario 256', Arial, sans-serif;
-         -webkit-text-stroke: 2px #000000;
-        }
-
-        h1 {
-          text-align: center;
-          font-size: 4rem;
-        }
-
-        h1:last-child {
-          font-size: 10rem;
-        }
-      }
-
-      button {
-        background-color: transparent;
-        border: none;
-        color: #FFFFFF;
-        font-size: 2.2rem;
-        font-weight: bolder;
-        margin-top: 1rem;
-        text-align: center;
-        width: 100%;
-        height: 3.5rem;
-
-        &:hover, &:active, &:focus {
-          background-color: #FFFFFF;
-          color: #00DDFF;
-          font-size: 2.2rem;
-          font-weight: bolder;
-          width: 100%;
-          border: none;
-          border-radius: 0.5rem;
-          box-shadow: 1px 3px 6px 0px rgba(60, 60, 60, 0.74);
-        }
-      }
     }
 
     .game-area {
@@ -308,21 +236,5 @@ export default {
         left (-($mountainsWidth * 1)) bottom $groundHeight,
         left 0 top 5rem;
     }
-  }
-
-  .red-color {
-    color: #FF0225;
-  }
-
-  .blue-color {
-    color: #00DDFF;
-  }
-
-  .yellow-color {
-    color: #FFC600;
-  }
-
-  .green-color {
-    color: #00D000;
   }
 </style>
