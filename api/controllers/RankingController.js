e7062c980d4a5e51b99049a89e911d0c13638552
EduardoVila <eduardodevila1@hotmail.com>
2020-10-07 14:22:51 -0300

    creating ranking features

--- api/controllers/RankingController.js
+++ api/controllers/RankingController.js
@@ -1,19 +1,19 @@
 const Ranking = require("../models/Ranking")
 
 exports.getHighestValues = (req, res) => {
-  Ranking.find({}, (err, task) => {
+  Ranking.find({}, (err, ranking) => {
     if (err) res.status(500).send(err)
 
-    res.status(200).json(task)
+    res.status(200).json(ranking)
   }).sort({age:-1}).limit(10)
 }
 
 exports.createNewRanking = (req, res) => {
   const newRanking = new Ranking(req.body)
 
-  newRanking.save((err, task) => {
+  newRanking.save((err, ranking) => {
     if (err) res.status(500).send(err)
 
-    res.status(201).json(task)
+    res.status(201).json(ranking)
   })
 }
